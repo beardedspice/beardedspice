fcabd509c638dbb6ff3485d2a0a386a91cf0c515
Breyten Ernsting <bje@dds.nl>
2014-01-16 14:45:44 +0100

    Implement strategy support for Shuffler.fm

---
+++ BeardedSpice/ShufflerFmStrategy.h
@@ -0,0 +1,15 @@
+//
+//  ShufflerFmStrategy.h
+//  BeardedSpice
+//
+//  Created by Breyten Ernsting on 1/16/14.
+//  Copyright (c) 2013 Tyler Rhodes / Jose Falcon. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface ShufflerFmStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+@end

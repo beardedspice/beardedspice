0e78b19fd9d2b53004677cab24920f90b4598172
Vladimir Burdukov <chippcheg@gmail.com>
2014-03-14 19:52:10 +0300

    Add YandexMusic support

---
+++ BeardedSpice/YandexMusicStrategy.h
@@ -0,0 +1,16 @@
+//
+//  YandexMusicStrategy.h
+//  BeardedSpice
+//
+//  Created by Vladimir Burdukov on 3/14/14.
+//  Copyright (c) 2014 Tyler Rhodes / Jose Falcon. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface YandexMusicStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+
+@end

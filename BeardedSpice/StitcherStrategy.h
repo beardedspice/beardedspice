cdc39a8e03793a2876b18623e2284752942ba8bc
Chris Williams <chrisbw@vrlsec.com>
2014-03-24 14:06:49 -0400

    Add Stitcher support

---
+++ BeardedSpice/StitcherStrategy.h
@@ -0,0 +1,15 @@
+//
+//  StitcherStrategy.h
+//  BeardedSpice
+//
+//  Created by Christopher Williams on 3/24/14.
+//  Copyright (c) 2014 Tyler Rhodes / Jose Falcon. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface StitcherStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+@end

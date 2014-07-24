a8309fc660bc4ffc109c873e21f865ca6ed63504
Brandon Smith <bsmith@w-bsmith.local>
2014-07-24 09:42:02 -0500

    Changed url condition in init.

--- BeardedSpice/AmazonMusicStrategy.m
+++ BeardedSpice/AmazonMusicStrategy.m
@@ -14,7 +14,7 @@
 {
     self = [super init];
     if (self) {
-        predicate = [NSPredicate predicateWithFormat:@"SELF LIKE[c] '*amazon.com/gp/dmusic/cloudplayer/player*'"];
+        predicate = [NSPredicate predicateWithFormat:@"SELF LIKE[c] '*amazon.com/gp/dmusic/cloudplayer/player*' OR '*amazon.com/gp/dmusic/cloudplayer/web*'"];
     }
     return self;
 }

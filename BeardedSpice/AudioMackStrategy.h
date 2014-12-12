7b50c8bcdef75c81469d88f6a6709ff4a085f82c
Sean Coker <seany789@gmail.com>
2014-12-12 01:44:17 -0500

    Add AudioMack strategy

---
+++ BeardedSpice/AudioMackStrategy.h
@@ -0,0 +1,15 @@
+//
+//  AudioMackStrategy.h
+//  BeardedSpice
+//
+//  Created by Sean Coker on 12/11/14.
+//  Copyright (c) 2014 Tyler Rhodes / Jose Falcon. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface AudioMackStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+@end

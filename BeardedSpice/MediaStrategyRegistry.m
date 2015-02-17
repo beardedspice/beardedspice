9feef255ea48b248f8029853427534ea15314a93
Alexander Chuprin <a.s.chuprin@gmail.com>
2015-02-17 10:05:17 +0300

    Odnoklassniki.ru support added

--- BeardedSpice/MediaStrategyRegistry.m
+++ BeardedSpice/MediaStrategyRegistry.m
@@ -39,6 +39,7 @@
 #import "DeezerStrategy.h"
 #import "FocusAtWillStrategy.h"
 #import "PocketCastsStrategy.h"
+#import "OdnoklassnikiStrategy.h"
 
 @interface MediaStrategyRegistry ()
 @property (nonatomic, strong) NSMutableDictionary *registeredCache;
@@ -181,7 +182,8 @@
                         [AudioMackStrategy new],
                         [DeezerStrategy new],
                         [FocusAtWillStrategy new],
-                        [PocketCastsStrategy new]
+                        [PocketCastsStrategy new],
+                        [OdnoklassnikiStrategy new]
                     ];
     });
     return strategies;
---
+++ BeardedSpice/OdnoklassnikiStrategy.m
@@ -0,0 +1,60 @@
+//
+//  OdnoklassnikiStrategy.m
+//  BeardedSpice
+//
+//  Created by Alexander Chuprin on 2/16/2015.
+//  Copyright (c) 2015 Alexander Chuprin. All rights reserved.
+//
+
+#import "OdnoklassnikiStrategy.h"
+
+@implementation OdnoklassnikiStrategy
+
+-(id) init
+{
+    self = [super init];
+    if (self) {
+        predicate = [NSPredicate predicateWithFormat:@"SELF LIKE[c] '*ok.ru*' OR SELF LIKE[c] '*odnoklassniki.ru*'"];
+    }
+    return self;
+}
+
+-(BOOL) accepts:(id <Tab>)tab
+{
+    return [predicate evaluateWithObject:[tab URL]];
+}
+
+-(NSString *) toggle
+{
+    return @"if (odklMusic.playingTrack() == \"\") {odklMusic.openAndLaunchMusicPlaying();} else {__getMusicFlash().lcPause();}";
+}
+
+-(NSString *) previous
+{
+    return @"__getMusicFlash().lcPrev()";
+}
+
+-(NSString *) next
+{
+    return @"__getMusicFlash().lcNext()";
+}
+
+-(NSString *) pause
+{
+    return @"__getMusicFlash().lcPause();";
+}
+
+-(NSString *) displayName
+{
+    return @"Odnoklassniki";
+}
+
+-(Track *) trackInfo:(id<Tab>)tab
+{
+    Track *track = [[Track alloc] init];
+    [track setTrack:[tab executeJavascript:@"document.querySelector('#mmpcw .mus_player_song').firstChild.nodeValue"]];
+    [track setArtist:[tab executeJavascript:@"document.querySelector('#mmpcw .mus_player_artist').firstChild.nodeValue"]];
+    return track;
+}
+
+@end

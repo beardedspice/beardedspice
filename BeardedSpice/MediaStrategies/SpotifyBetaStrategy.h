ef37000ce9506437b1da50cfe7c7791778f99d65
Romain Brucker <romain.brucker@amalto.com>
2015-07-23 12:05:01 -0500

    Adding a MediaStrategy for Spotify's new player called SpotifyBeta

---
+++ BeardedSpice/MediaStrategies/SpotifyBetaStrategy.h
@@ -0,0 +1,17 @@
+//
+//  SpotifyBetaStrategyBeta.h
+//  BeardedSpice
+//
+//  Created by Azorr on 7/23/2015.
+//  Copyright (c) 2013 Tyler Rhodes / Jose Falcon. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface SpotifyBetaStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+
+@end
+

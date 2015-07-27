8674c04c7854e0858d6e2dd3818604ad071301d7
Pascal <pascal@blitzr.com>
2015-07-27 09:01:06 +0200

    add media strategy for Blitzr.com

---
+++ BeardedSpice/MediaStrategies/BlitzrStrategy.h
@@ -0,0 +1,16 @@
+//
+//  BlitzrStrategy.h
+//  BeardedSpice
+//
+//  Created by pascal Fouque on 23/07/2015.
+//  Copyright (c) 2015 BeardedSpice. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface BlitzrStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+
+@end

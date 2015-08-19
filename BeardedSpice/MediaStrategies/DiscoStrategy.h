e811f3cca0529a839966fed4d1aad0330fc47139
Colin Drake <colin.f.drake@gmail.com>
2015-08-19 17:27:13 -0400

    Adds in disco.io strategy.

---
+++ BeardedSpice/MediaStrategies/DiscoStrategy.h
@@ -0,0 +1,16 @@
+//
+//  DiscoStrategy.h
+//  BeardedSpice
+//
+//  Created by Colin Drake on 8/19/15.
+//  Copyright (c) 2015 BeardedSpice. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface DiscoStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+
+@end

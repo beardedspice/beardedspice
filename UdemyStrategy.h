c85ab3bd362119a9c8b0f2ca6e84d31743c913de
Coder256 <Coder-256@users.noreply.github.com>
2015-10-11 16:42:43 -0400

    Update CocoaPods version and fix paths in project.pbxproj

---
+++ UdemyStrategy.h
@@ -0,0 +1,16 @@
+//
+//  UdemyStrategy.h
+//  BeardedSpice
+//
+//  Created by Coder-256 on 10/3/15.
+//  Copyright © 2015 BeardedSpice. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface UdemyStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+
+@end

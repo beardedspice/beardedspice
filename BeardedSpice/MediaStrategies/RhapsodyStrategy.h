1d0073b70dbb5aa397641e11a82a768639e552e0
Aaron Pollack <pollackaaron@gmail.com>
2015-11-18 14:43:32 -0800

    Closes #279 - Rhapsody Support
        - Add toggle, like, play, next, and previous
        - Friendly album names are not gaurenteed,
        but attempt is made to get it

---
+++ BeardedSpice/MediaStrategies/RhapsodyStrategy.h
@@ -0,0 +1,16 @@
+//
+//  RhapsodyStrategy.h
+//  BeardedSpice
+//
+//  Created by Aaron Pollack on 11/17/15.
+//  Copyright © 2015 BeardedSpice. All rights reserved.
+//
+
+#import "MediaStrategy.h"
+
+@interface RhapsodyStrategy : MediaStrategy
+{
+    NSPredicate *predicate;
+}
+
+@end

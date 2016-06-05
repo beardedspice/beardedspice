fb91abbd6b1ea867905e5496227a782208ef6d97
Alex Evers <alex.evers2@gmail.com>
2016-06-05 15:27:21 -0700

    Formatting fix for Deezer Strategy

--- BeardedSpice/MediaStrategies/Deezer.js
+++ BeardedSpice/MediaStrategies/Deezer.js
@@ -7,30 +7,28 @@
 //
 
 BSStrategy = {
+  version:2,
+  displayName:"Deezer",
+  accepts: {
+    method: "predicateOnTab",
+    format:"%K LIKE[c] '*deezer.com*'",
+    args: ["URL"]
+  },
+  isPlaying: function(){return dzPlayer.isPlaying();},
+  toggle: function () {dzPlayer.control.togglePause()},
+  next: function () {dzPlayer.control.nextSong()},
+  favorite: function (){document.querySelector('div.player-actions span.icon-love').click()},
+  previous: function () {dzPlayer.control.prevSong()},
+  pause: function () {dzPlayer.control.pause()},
 
-version:2,
-displayName:"Deezer",
-accepts: {
-method: "predicateOnTab",
-format:"%K LIKE[c] '*deezer.com*'",
-args: ["URL"]
-},
-isPlaying: function(){return dzPlayer.isPlaying();},
-toggle: function () {dzPlayer.control.togglePause()},
-next: function () {dzPlayer.control.nextSong()},
-favorite: function (){document.querySelector('div.player-actions span.icon-love').click()},
-previous: function () {dzPlayer.control.prevSong()},
-pause: function () {dzPlayer.control.pause()},
-    
-trackInfo: function () {
-    var info = dzPlayer.getCurrentSong();
-    return {
-        'track': info["SNG_TITLE"] + (info["VERSION"] == "" ? "" : " " + info["VERSION"]),
-        'album': info["ALB_TITLE"],
-        'artist': dzPlayer.getArtistName(),
-        'image': 'http://cdn-images.deezer.com/images/cover/' + info["ALB_PICTURE"]+ '/250x250.jpg',
-        'favorited': userData.isFavorite('song',info["SNG_ID"])
-    };
-}
-    
+  trackInfo: function () {
+      var info = dzPlayer.getCurrentSong();
+      return {
+          'track': info["SNG_TITLE"] + (info["VERSION"] == "" ? "" : " " + info["VERSION"]),
+          'album': info["ALB_TITLE"],
+          'artist': dzPlayer.getArtistName(),
+          'image': 'http://cdn-images.deezer.com/images/cover/' + info["ALB_PICTURE"]+ '/250x250.jpg',
+          'favorited': userData.isFavorite('song',info["SNG_ID"])
+      };
+  }
 }

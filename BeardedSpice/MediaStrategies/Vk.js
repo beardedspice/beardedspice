d5f38376b6f394d848777061d17cc11f935232a5
Alex Evers <alex.evers2@gmail.com>
2016-06-06 14:41:26 -0700

    Syntax fix for Vk Strategy. Needs validation that it still works.

--- BeardedSpice/MediaStrategies/Vk.js
+++ BeardedSpice/MediaStrategies/Vk.js
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Anton Mihailov. All rights reserved.
 //
 BSStrategy = {
-  version:1,
+  version: 1,
   displayName:"VK",
   accepts: {
     method: "predicateOnTab",
@@ -14,52 +14,59 @@ BSStrategy = {
     args: ["URL"]
   },
   isPlaying: function () { !!document.querySelector('#ac_play.playing, #gp_play.playing'); },
-  toggle: function (w) {
-    var el = document.querySelector('#ac_play, #gp_play');
-    if (el) { el.click(); return; }
-    w.Pads.show('mus', null);
-    var pollPlayerInterval = setInterval(
-    (function(w){
-        return function(){
-            var el = document.querySelector('#pd_play');
-            if (!el) { return; }
-            clearInterval(pollPlayerInterval);
-            el.click();
-            w.Pads.hide('mus', null);
-        }
-    })(w),10);
-  }(window),
-  next: function (w) {
-    var el = document.querySelector('#ac_next');
-    if (el) { el.click(); return; }
-    w.Pads.show('mus', null);
-    var pollPlayerInterval = setInterval(
-    (function(w){
-        return function(){
-            var el = document.querySelector('#pd_next');
-            if (!el) { return; }
-            clearInterval(pollPlayerInterval);
-            el.click();
-            w.Pads.hide('mus', null);
-        }
-    })(w), 10);
-  }(window),
-  favorite: function (w) {
-    var el = document.querySelector('#ac_add');
-    if (el) { el.click(); return; }
-    w.Pads.show('mus', null);
-    var pollPlayerInterval = setInterval(
-    (function(w){
-        return function(){
-            var el = document.querySelector('#pd_add');
-            if (!el) { return; }
-            clearInterval(pollPlayerInterval);
-            el.click();
-            w.Pads.hide('mus', null);
-        }
-    })(w), 10);
-  }(window),
-  previous: function (w) {
+  toggle: function () {
+    (function (w) {
+      var el = document.querySelector('#ac_play, #gp_play');
+      if (el) { el.click(); return; }
+      w.Pads.show('mus', null);
+      var pollPlayerInterval = setInterval(
+      (function(w){
+          return function(){
+              var el = document.querySelector('#pd_play');
+              if (!el) { return; }
+              clearInterval(pollPlayerInterval);
+              el.click();
+              w.Pads.hide('mus', null);
+          }
+      })(w),10);
+    }(window))
+  },
+  next: function () {
+    (function (w) {
+      var el = document.querySelector('#ac_next');
+      if (el) { el.click(); return; }
+      w.Pads.show('mus', null);
+      var pollPlayerInterval = setInterval(
+      (function(w){
+          return function(){
+              var el = document.querySelector('#pd_next');
+              if (!el) { return; }
+              clearInterval(pollPlayerInterval);
+              el.click();
+              w.Pads.hide('mus', null);
+          }
+      })(w), 10);
+    }(window))
+  },
+  favorite: function () {
+    (function (w) {
+      var el = document.querySelector('#ac_add');
+      if (el) { el.click(); return; }
+      w.Pads.show('mus', null);
+      var pollPlayerInterval = setInterval(
+      (function(w){
+          return function(){
+              var el = document.querySelector('#pd_add');
+              if (!el) { return; }
+              clearInterval(pollPlayerInterval);
+              el.click();
+              w.Pads.hide('mus', null);
+          }
+      })(w), 10);
+    }(window))
+  },
+  previous: function () {
+    (function (w) {
       var el = document.querySelector('#ac_prev');
       if (el) { el.click(); return; }
       w.Pads.show('mus', null);
@@ -72,8 +79,10 @@ BSStrategy = {
               w.Pads.hide('mus', null);
           }
       })(w), 10);
-  }(window),
-  pause: function (w) {
+    }(window))
+  },
+  pause: function () {
+    (function (w) {
       var el = document.querySelector('#ac_play.playing, #gp_play.playing');
       if (el) { el.click(); return; }
       w.Pads.show('mus', null);
@@ -88,8 +97,10 @@ BSStrategy = {
           }
       })(w), 10);
       setTimeout(function(){clearInterval(pollPlayerInterval);}, 1000);
-    }(window),
-  trackInfo: function (w) {
+    }(window))
+  },
+  trackInfo: function () {
+    (function (w) {
       var titleEl = document.querySelector('span#ac_title, #gp_title');
       var artistEl = document.querySelector('span#ac_performer, #gp_performer');
       if (! titleEl || ! artistEl) {
@@ -105,5 +116,6 @@ BSStrategy = {
           'title': titleEl.firstChild.nodeValue,
           'artist': artistEl.firstChild.nodeValue
       };
-  }(window),
+    }(window))
+  },
 }

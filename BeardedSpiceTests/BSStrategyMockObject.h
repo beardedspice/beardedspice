846f0df4c4b524ab298080090a25749b17ad8d21
Alex Evers <alex.evers2@gmail.com>
2016-08-28 11:56:30 -0700

    Added explanation for BSStrategyMockObject

--- BeardedSpiceTests/BSStrategyMockObject.h
+++ BeardedSpiceTests/BSStrategyMockObject.h
@@ -10,6 +10,12 @@
 
 typedef void (^BSVoidBlock)(void);
 
+/**
+ This object is intended to simulate a browser environment that executes/tests
+ the individual strategies. The environment would load a cached copy of each
+ website and inject the strategy code to test for response. This also would
+ provide a test against future media site changes that break existing strategies.
+*/
 @interface BSStrategyMockObject : NSObject <WebFrameLoadDelegate>
 
 @property (nonatomic, assign) BOOL finishedLoading;

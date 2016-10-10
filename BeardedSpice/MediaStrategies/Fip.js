355865ec199a1a4731af41e3adc4c448b826816f
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-10-10 21:36:30 +0200

    Add support for Fip Player

---
+++ BeardedSpice/MediaStrategies/Fip.js
@@ -0,0 +1,28 @@
+//
+//  YouTube.plist
+//  BeardedSpice
+//
+//  Created by Jose Falcon on 12/15/13.
+//  Copyright (c) 2013 Tyler Rhodes / Jose Falcon. All rights reserved.
+//
+BSStrategy = {
+  version: 1,
+  displayName: "Fip",
+  accepts: {
+    method: "predicateOnTab",
+    format: "%K LIKE[c] '*fipradio.fr/player*'",
+    args: ["URL"]
+  },
+  isPlaying: function () { return (document.querySelector('#player-controls .stop') !== null); },
+  toggle: function () { document.querySelector('#player-controls button').click();; },
+  previous: function () { },
+  next: function () { },
+  pause: function () { document.querySelector('#player-controls button').click(); },
+  trackInfo: function () {
+    return {
+      'image': document.querySelector('.cover .picture').getAttribute('src'),
+      'track': document.querySelector('.infos .title').getAttribute('title'),
+      'artist': document.querySelector('.infos .author .name').getAttribute('title')
+    };
+  }
+}

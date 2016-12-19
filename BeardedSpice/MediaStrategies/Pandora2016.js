6d8d1c53df09f965cddc9875c77a31553ab59889
Bret Martin <bam@miranda.org>
2016-12-19 15:02:47 -0500

    Add second Pandora strategy for late 2016 interface update

---
+++ BeardedSpice/MediaStrategies/Pandora2016.js
@@ -0,0 +1,35 @@
+//
+//  Pandora2016.js
+//  BeardedSpice
+//
+//  Created by Bret Martin on 2016-12-19
+//
+//  Copyright (c) 2015 GPL v3 http://www.gnu.org/licenses/gpl.html
+//
+
+BSStrategy = {
+  version: 1,
+  displayName: "Pandora (late 2016)",
+  accepts: {
+    method: "predicateOnTab",
+    format: "%K LIKE[c] '*pandora.com*'",
+    args: ["URL"]
+  },
+
+  isPlaying: function () {
+    document.querySelector('.Tuner__Control__Play__Button').attributes['data-qa'].value === 'play_button'
+  },
+  toggle: function () { document.querySelector('.Tuner__Control__Play__Button').click(); },
+  next: function () { document.querySelector('.Tuner__Control__Skip__Button').click(); },
+  pause: function () { document.querySelector('.Tuner__Control__Play__Button').click(); },
+  favorite: function() { document.querySelector('.Tuner__Control__ThumbUp__Button').click(); },
+  trackInfo: function () {
+    return {
+      'track': document.querySelector('.nowPlayingTopInfo__current__trackName .Marquee__wrapper__content__child').innerText,
+      'artist': document.querySelector('.nowPlayingTopInfo__current__artistName').innerText,
+      'album': document.querySelector('.nowPlayingTopInfo__current__albumName').innerText,
+      'image': document.querySelector('[data-qa=album_active_image').style['background-image'].slice(5, -2),
+      'favorited': document.querySelector('[data-qa=thumbs_up_button]').classList.contains('ThumbUpButton--active')
+    };
+  }
+}

0a812ae84ca3c2011be1934c3b248e56c1bbbaac
Bret Martin <bam@miranda.org>
2017-01-01 00:45:31 -0500

    Pandora: add support for optional new UI

--- BeardedSpice/MediaStrategies/Pandora.js
+++ BeardedSpice/MediaStrategies/Pandora.js
@@ -15,31 +15,70 @@ BSStrategy = {
     args: ["URL"]
   },
   isPlaying: function () {
-    return document.querySelector('.pauseButton').style.display === 'block';
+    if (document.querySelector('div.Tuner__Controls') !== null) {
+      return
+        document
+          .querySelector('.Tuner__Control__Play__Button')
+          .attributes['data-qa']
+          .value === 'play_button';
+    } else {
+      return document.querySelector('.pauseButton').style.display === 'block';
+    }
   },
   toggle: function () {
-    var playButton = document.querySelector('.playButton');
-    var pauseButton = document.querySelector('.pauseButton');
-    if (playButton.style.display==='block') { playButton.click() }
-    else { pauseButton.click() }
+    if (document.querySelector('div.Tuner__Controls') !== null) {
+      document.querySelector('.Tuner__Control__Play__Button').click();
+    } else {
+      var playButton = document.querySelector('.playButton');
+      var pauseButton = document.querySelector('.pauseButton');
+      if (playButton.style.display==='block') { playButton.click() }
+      else { pauseButton.click() }
+    }
   },
   next: function () {
+    document.querySelector('div.Tuner__Controls') !== null ?
+    document.querySelector('.Tuner__Control__Skip__Button').click() :
     document.querySelector('.skipButton').click();
   },
   pause: function () {
+    document.querySelector('div.Tuner__Controls') !== null ?
+    document.querySelector('.Tuner__Control__Play__Button').click() :
     document.querySelector('.pauseButton').click();
   },
   favorite: function () {
+    document.querySelector('div.Tuner__Controls') !== null ?
+    document.querySelector('.Tuner__Control__ThumbUp__Button').click() :
     document.querySelector('.thumbUpButton').click();
   },
   trackInfo: function () {
-    return {
-      'track': document.querySelector('.playerBarSong').innerText,
-      'artist': document.querySelector('.playerBarArtist').innerText,
-      'album': document.querySelector('.playerBarAlbum').innerText,
-      'image': document.querySelector('.playerBarArt').src,
-      'favorited': document.querySelector('.thumb').style.display === 'block' &&
-                   document.querySelector('.thumb').id === 'thumbup'
+    if (document.querySelector('div.Tuner__Controls') !== null) {
+      return {
+        'track': document
+                   .querySelector('div.Tuner__Audio__TrackDetail__title')
+                   .innerText,
+        'artist': document
+                    .querySelector('div.Tuner__Audio__TrackDetail__artist')
+                    .innerText,
+        'album': document
+                   .querySelector('.nowPlayingTopInfo__current__albumName')
+                   .innerText,
+        'image': document
+                   .querySelector('[data-qa=album_active_image]')
+                   .style['background-image']
+                   .slice(5, -2),
+        'favorited': document
+                       .querySelector('[data-qa=thumbs_up_button]')
+                       .classList.contains('ThumbUpButton--active')
+      };
+    } else {
+      return {
+        'track': document.querySelector('.playerBarSong').innerText,
+        'artist': document.querySelector('.playerBarArtist').innerText,
+        'album': document.querySelector('.playerBarAlbum').innerText,
+        'image': document.querySelector('.playerBarArt').src,
+        'favorited': document.querySelector('.thumb').style.display === 'block' &&
+                     document.querySelector('.thumb').id === 'thumbup'
+      };
     };
   }
 }

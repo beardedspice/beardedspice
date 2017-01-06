b46f84d7fdd09d1ebf49c1fc6566c3f32a6fb70f
Frank Febbraro <frank@phase2technology.com>
2017-01-06 15:13:40 -0800

    fixed formatting

--- BeardedSpice/MediaStrategies/PhishIn.js
+++ BeardedSpice/MediaStrategies/PhishIn.js
@@ -6,23 +6,23 @@
 //  Copyright (c) 2017 Frank Febbraro. All rights reserved.
 //
 BSStrategy = {
-version: 1,
-displayName: "PhishIn",
-accepts: {
-method: "predicateOnTab",
-format: "%K LIKE[c] '*phish.in*'",
-args: ["URL"]
-},
-isPlaying: function () { return document.querySelector('#control_playpause.playing') !== null; },
-toggle: function () { document.querySelector('#control_playpause').click(); },
-previous: function () { document.querySelector('#control_previous').click(); },
-next: function () { document.querySelector('#control_next').click(); },
-pause: function () { document.querySelector('#control_playpause.playing').click(); },
-trackInfo: function () {
-    return {
-        'track': document.querySelector('#player_title').innerText,
-        'album': document.querySelector('#player_detail').innerText,
-        'artist': 'Phish'
-    };
-}
+    version: 1,
+    displayName: "PhishIn",
+    accepts: {
+        method: "predicateOnTab",
+        format: "%K LIKE[c] '*phish.in*'",
+        args: ["URL"]
+    },
+    isPlaying: function () { return document.querySelector('#control_playpause.playing') !== null; },
+    toggle: function () { document.querySelector('#control_playpause').click(); },
+    previous: function () { document.querySelector('#control_previous').click(); },
+    next: function () { document.querySelector('#control_next').click(); },
+    pause: function () { document.querySelector('#control_playpause.playing').click(); },
+    trackInfo: function () {
+        return {
+            'track': document.querySelector('#player_title').innerText,
+            'album': document.querySelector('#player_detail').innerText,
+            'artist': 'Phish'
+        };
+    }
 }

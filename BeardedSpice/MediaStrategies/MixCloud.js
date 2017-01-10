7163bc9ad0acfb32b6df44034e6f59a5dd59016f
George Davaris <george@mixcloud.com>
2017-01-10 14:22:12 +0000

    Remove Mixcloud Beta media strategy and update Mixcloud

--- BeardedSpice/MediaStrategies/MixCloud.js
+++ BeardedSpice/MediaStrategies/MixCloud.js
@@ -1,9 +1,6 @@
 //
-//  MixCloud.plist
+//  Mixcloud.plist
 //  BeardedSpice
-//
-//  Created by Tyler Rhodes on 2/23/14.
-//  Copyright (c) 2014 Tyler Rhodes / Jose Falcon. All rights reserved.
 //
 BSStrategy = {
   version:3,

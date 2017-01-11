45ac7b87218fbfc54fb8ffbf8eef568fced9758a
George Davaris <george@mixcloud.com>
2017-01-11 18:43:05 +0000

    Revert rename

--- BeardedSpice/MediaStrategies/MixCloud.js
+++ BeardedSpice/MediaStrategies/MixCloud.js
@@ -1,9 +1,12 @@
 //
-//  Mixcloud.plist
+//  MixCloud.plist
 //  BeardedSpice
 //
+//  Created by Tyler Rhodes on 2/23/14.
+//  Copyright (c) 2014 Tyler Rhodes / Jose Falcon. All rights reserved.
+//
 BSStrategy = {
-  version:4,
+  version:3,
   displayName:"Mixcloud",
   accepts: {
     method: "predicateOnTab",

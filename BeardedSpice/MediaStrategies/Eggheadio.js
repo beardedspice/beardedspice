237c89c1d8e6f68ee03998a3b350a42d080afd90
Carlos Filoteo <cfiloteo@ancestry.com>
2017-03-20 19:03:14 -0600

    Add Egghead.io media strategy

---
+++ BeardedSpice/MediaStrategies/Eggheadio.js
@@ -0,0 +1,41 @@
+//
+//  Eggheadio.plist
+//  BeardedSpice
+//
+//  Created by Carlos Filoteo on 3/20/17.
+//  Copyright (c) 2017 GPL v3 http://www.gnu.org/licenses/gpl.html
+//
+BSStrategy = {
+  version:1,
+  displayName:"Eggheadio",
+  accepts: {
+    method: "predicateOnTab",
+    format:"%K LIKE[c] '*egghead.io/lessons*'",
+    args: ["URL"]
+  },
+  isPlaying:function () {
+    return Wistia.api("wistia_").state() === "playing";
+  },
+  toggle: function () {
+    var video = Wistia.api("wistia_");
+    video.state() === "playing" ? video.pause() : video.play();
+  },
+  previous: function () {
+    var e = document.querySelector(".up-next-list-item.current").parentElement.previousSibling;
+    if(e){ e.childNodes[0].click(); }
+  },
+  next: function () {
+    var e = document.querySelector(".up-next-list-item.current").parentElement.nextSibling;
+    if(e){ e.childNodes[1].click(); }
+  },
+  pause:function () {
+    Wistia.api("wistia_").pause();
+  },
+  trackInfo: function () {
+    return {
+      "track": document.querySelector("meta[itemprop=name]").getAttribute("content"),
+      "artist": window.instructor,
+      "image": document.querySelector("meta[itemprop=image]").getAttribute("content")
+    }
+  }
+}

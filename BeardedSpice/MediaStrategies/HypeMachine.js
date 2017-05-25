fceeeb8ada1c8df2a961ea1929d900bf97073a43
nVitius <mauricio@nviti.us>
2017-05-25 11:51:27 -0700

    Use latest queries in strategy for HypeMachine

--- BeardedSpice/MediaStrategies/HypeMachine.js
+++ BeardedSpice/MediaStrategies/HypeMachine.js
@@ -3,21 +3,23 @@
 //  BeardedSpice
 //
 //  Created by Jose Falcon on 12/16/13.
+//  Updated to v2 by nVitius on 05/25/17.
 //  Copyright (c) 2013 Tyler Rhodes / Jose Falcon. All rights reserved.
 //
 BSStrategy = {
-  version:1,
-  displayName:"HypeMachine",
+  version: 2,
+  displayName: "HypeMachine",
   accepts: {
     method: "predicateOnTab",
     format:"%K LIKE[c] '*hypem.com*'",
     args: ["URL"]
   },
-  toggle: function () {return window.togglePlay()},
-  next: function () {return window.nextTrack()},
-  previous: function () {return window.prevTrack()},
-  favorite: function (){return window.toggleFavoriteItem()},
-  pause: function () {return window.currentPlayerObj[0].pause()},
+  isPlaying: function () { return document.querySelector('#playerPlay.pause') !== null; },
+  toggle: function () { document.querySelector('#playerPlay').click(); },
+  next: function () { document.querySelector('#playerNext').click(); },
+  previous: function () { document.querySelector('#playerPrev').click(); },
+  favorite: function (){ document.querySelector('#playerFav').click(); },
+  pause: function () { document.querySelector('#playerPlay.pause').click(); },
   trackInfo: function () {
     return {
       'artist': now_playing[0].text,

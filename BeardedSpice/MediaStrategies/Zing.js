b527d00ed37b634916b3b58074eb451c4269e923
ToanPVNSE03821 <toanpvnse03821@fpt.edu.vn>
2017-08-23 12:57:33 +0700

    Update Zing.js
    Update for Zing MP3 23/08/2017

--- BeardedSpice/MediaStrategies/Zing.js
+++ BeardedSpice/MediaStrategies/Zing.js
@@ -3,19 +3,25 @@
 //  BeardedSpice
 //
 //  Created by Alvin Nguyen on 06/23/16.
+//  Updated by ToanPVN on 08/23/17.
 //
 BSStrategy = {
-  version: 1,
+  version: 3,
   displayName: "Zing MP3",
   accepts: {
     method: "predicateOnTab",
     format: "%K LIKE[c] '*mp3.zing.vn/*'",
     args: ["URL"]
   },
-toggle: function(){  if (document.querySelector('.jp-play').style.display === 'none') { document.querySelector('.jp-pause').click(); } else { document.querySelector('.jp-play').click();} },
+  isPlaying: function () { return document.querySelector('#zp-svg-play') == null; },
+  toggle: function(){ document.querySelector('.paused').click(); },
   previous: function(){ document.querySelector('.fn-prev').click(); },
-  next: function(){ document.querySelector('.fn-next').click(); },
-  pause: function(){ document.querySelector('.jp-pause').click(); },
+  next: function(){ document.querySelector('.fn-list li:first-child a.thumb').click(); },
+  pause: function(){ 
+    if (document.querySelector('#zp-svg-play') == null) {
+      document.querySelector('.paused').click();
+    }
+  },
   trackInfo: function () {
     return {
         'image': document.querySelector('.pthumb').getAttribute('src'),

d37a97d29d833f83f9ec6d8d89c4665bbcf11cac
MikeWLloyd <lloydm@si.edu>
2017-09-05 07:48:52 -0400

    converted to two spaces

--- BeardedSpice/MediaStrategies/Weta.js
+++ BeardedSpice/MediaStrategies/Weta.js
@@ -5,34 +5,34 @@
 //
 
 BSStrategy = {
-    version: 1,
-    displayName: "WETA",
-    accepts: {
-        method: "predicateOnTab",
-        format: "%K LIKE[c] '*weta.org/listen-live*'",
-        args: ["URL"]
-    },
+  version: 1,
+  displayName: "WETA",
+  accepts: {
+    method: "predicateOnTab",
+    format: "%K LIKE[c] '*weta.org/listen-live*'",
+    args: ["URL"]
+  },
 
-    isPlaying:function () {
-        var e = document.querySelector('.jp-pause');
-		return e.style.display === 'block';
-    },
-    toggle: function () {
-   		var e = document.querySelector('.jp-pause');
-		if(e.style.display === 'block') {
-    		document.querySelector('.jp-pause').click();
-    	} else {
-    		document.querySelector('.jp-play').click();
-    	}
-    },
-    next: function () {},
-    previous: function () {},
-    favorite: function () {},
-    pause: function () {document.querySelector('.jp-pause').click()},
-    trackInfo: function () {
-        return {
-            'track':  document.querySelector('.jp-type-single p').innerText.replace(/\"/g,''),
-            'artist': document.querySelector('.jp-type-single h5').innerText.replace(/\"/g,'')
-        }
+  isPlaying:function () {
+    var e = document.querySelector('.jp-pause');
+    return e.style.display === 'block';
+  },
+  toggle: function () {
+    var e = document.querySelector('.jp-pause');
+    if(e.style.display === 'block') {
+      document.querySelector('.jp-pause').click();
+    } else {
+      document.querySelector('.jp-play').click();
     }
+  },
+  next: function () {},
+  previous: function () {},
+  favorite: function () {},
+  pause: function () {document.querySelector('.jp-pause').click()},
+  trackInfo: function () {
+    return {
+      'track':  document.querySelector('.jp-type-single p').innerText.replace(/\"/g,''),
+      'artist': document.querySelector('.jp-type-single h5').innerText.replace(/\"/g,'')
+    }
+  }
 }

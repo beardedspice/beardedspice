912874d060201416dde38cef85101aafe24e60bb
Carlos Filoteo <cfiloteo@ancestry.com>
2017-11-14 16:21:26 -0700

    Add PocketCastsBeta media strategy

---
+++ BeardedSpice/MediaStrategies/PocketCastsBeta.js
@@ -0,0 +1,27 @@
+//
+//  PocketCastsBeta.plist
+//  BeardedSpice
+//
+//  Created by Carlos Filoteo on 11/14/17.
+//  Copyright (c) 2017 GPL v3 http://www.gnu.org/licenses/gpl.html
+//
+BSStrategy = {
+  version:1,
+  displayName:"PocketCastsBeta",
+  accepts: {
+    method: "predicateOnTab",
+    format:"%K LIKE[c] '*playbeta.pocketcasts.com*'",
+    args: ["URL"]
+  },
+  toggle: function () {document.querySelector('.play_pause_button').click()},
+  next: function () {document.querySelector('.skip_forward_button').click()},
+  previous: function () {document.querySelector('.skip_back_button').click()},
+  pause: function () {document.querySelector('.pause_button').click()},
+  trackInfo: function () {
+    return {
+      'track': document.querySelector('.episode-title').innerText,
+      'album': document.querySelector('.podcast-title').innerText,
+      'image': document.querySelector('.podcast-image img').src,
+    }
+  }
+}

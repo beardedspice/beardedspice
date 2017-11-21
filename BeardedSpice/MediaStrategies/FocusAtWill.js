dcd2223e156d6630901b81215b7fd576a73e5dfe
Jayphen <jayphen@gmail.com>
2017-11-21 20:03:56 +0100

    Update focus@will strategy to retrieve track info

--- BeardedSpice/MediaStrategies/FocusAtWill.js
+++ BeardedSpice/MediaStrategies/FocusAtWill.js
@@ -5,6 +5,7 @@
 //  Created by Jayphen on 11/21/17.
 //  Copyright (c) 2017 GPL v3 http://www.gnu.org/licenses/gpl.html
 //
+
 BSStrategy = {
   version:2,
   displayName:"focus@will",
@@ -16,7 +17,27 @@ BSStrategy = {
   isPlaying: function () {
     return window.faw.isPlaying
   },
-  toggle: function () {window.faw.play()},
-  next: function () { document.querySelector('a.next').click() },
-  pause: function () { document.querySelector('a.play').click() }
+  next: function () { window.faw.skip() },
+  toggle: function () {
+    window.faw.isPlaying ? window.faw.stop() : window.faw.play()
+  },
+  trackInfo: function () {
+    var FindReact = function(dom) {
+        for (var key in dom) {
+            if (key.startsWith("__reactInternalInstance$")) {
+                var compInternals = dom[key]._currentElement;
+                var compWrapper = compInternals._owner;
+                var comp = compWrapper._instance;
+                return comp;
+            }
+        }
+        return null;
+    };
+    var footerDiv = document.querySelector('header + footer div:first-child');
+    var trackInfo = FindReact(footerDiv).props.currentTrack;
+    return {
+      "track": trackInfo.trackName,
+      "artist": trackInfo.artistName,
+    };
+  }
 }

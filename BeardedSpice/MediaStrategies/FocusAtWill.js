51a2871690e2df48138095332ce8b5822d6d5046
Jayphen <jayphen@gmail.com>
2017-11-21 19:35:23 +0100

    Update focus@will strategy

--- BeardedSpice/MediaStrategies/FocusAtWill.js
+++ BeardedSpice/MediaStrategies/FocusAtWill.js
@@ -2,18 +2,21 @@
 //  FocusAtWill.plist
 //  BeardedSpice
 //
-//  Created by Ken Mickles on 1/15/15.
-//  Copyright (c) 2015 Tyler Rhodes / Jose Falcon. All rights reserved.
+//  Created by Jayphen on 11/21/17.
+//  Copyright (c) 2017 GPL v3 http://www.gnu.org/licenses/gpl.html
 //
 BSStrategy = {
-  version:1,
+  version:2,
   displayName:"focus@will",
   accepts: {
     method: "predicateOnTab",
     format:"%K LIKE[c] '*focusatwill.com*'",
     args: ["URL"]
   },
-  toggle: function () {document.querySelector('a.play').click()},
+  isPlaying: function () {
+    return window.faw.isPlaying
+  },
+  toggle: function () {window.faw.play()},
   next: function () { document.querySelector('a.next').click() },
   pause: function () { document.querySelector('a.play').click() }
 }

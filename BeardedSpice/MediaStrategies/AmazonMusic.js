cd7e903b8aef43c5056ee591d08f94f0a48f009b
Matteo Gaggiano <maxxxx92@gmail.com>
2018-06-20 12:46:29 +0200

    Enhanced support for Amazon Music web
    Added controls from querySelector enhancing the already present one.

--- BeardedSpice/MediaStrategies/AmazonMusic.js
+++ BeardedSpice/MediaStrategies/AmazonMusic.js
@@ -3,23 +3,53 @@
 //  BeardedSpice
 //
 //  Created by Brandon P Smith on 7/23/14.
+//  Edited by Matteo Gaggiano on 6/20/2018.
 //  Copyright (c) 2014 Tyler Rhodes / Jose Falcon. All rights reserved.
 //
 BSStrategy = {
-  version:1,
+  version:2,
   displayName:"Amazon Music",
   accepts: {
     method: "predicateOnTab",
     format:"%K LIKE[c] '*music.amazon.*'",
     args: ["URL"]
   },
-  isPlaying: function () {return window.amznMusic.widgets.player.isPlaying();},
-  toggle: function () {return window.amznMusic.widgets.player.playHash('togglePlay')},
-  next: function () {return window.amznMusic.widgets.player.playHash('next')},
-  previous: function () {return window.amznMusic.widgets.player.playHash('previous')},
-  pause: function () {window.amznMusic.widgets.player.pause();},
+  isPlaying: function () {
+    if (window.amznMusic)
+      return window.amznMusic.widgets.player.isPlaying();
+    return window.document.querySelector('.playbackActive .playbackControls .button.playerIcon.playButton.playerIconPause') !== null;
+  },
+  toggle: function () {
+    if (window.amznMusic)
+      return window.amznMusic.widgets.player.playHash('togglePlay');
+    return window.document.querySelector('.playbackActive .playbackControls .button.playerIcon.playButton').click();
+  },
+  next: function () {
+    if (window.amznMusic)
+      return window.amznMusic.widgets.player.playHash('next');
+    return window.document.querySelector('.playbackActive .playbackControls .button.nextButton').click();
+  },
+  previous: function () {
+    if (window.amznMusic)
+      return window.amznMusic.widgets.player.playHash('previous');
+    return window.document.querySelector('.playbackActive .playbackControls .button.previousButton').click();
+  },
+  pause: function () {
+    if (window.amznMusic)
+      window.amznMusic.widgets.player.pause();
+    else
+      window.document.querySelector('.playbackActive .playbackControls .button.playerIcon.playButton.playerIconPause').click();
+  },
   trackInfo: function () {
-    var data = window.amznMusic.widgets.player.getCurrent()['metadata'];
+    var data = {};
+    if (window.amznMusic) {
+      data = window.amznMusic.widgets.player.getCurrent()['metadata'];
+    } else {
+      data['title'] = window.document.querySelector('.trackTitle a').getAttribute('title');
+      data['albumName'] = window.document.querySelector('.trackArtist a').getAttribute('title');
+      data['artistName'] = window.document.querySelector('.trackSourceLink a').innerText;
+      data['albumCoverImageSmall'] = window.document.querySelector('.renderImage').getAttribute('src');
+    }
     return {
         'track': data['title'],
         'artist': data['albumName'],

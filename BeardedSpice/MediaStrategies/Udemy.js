d5874a77858b6abc413a5d80a3632b16f6777e51
Mindaugas Bujanauskas <bujanauskas.m@gmail.com>
2018-07-18 10:56:17 +0300

    Updated the Udemy strategy
    Udemy has renamed their video component from `video-player` to `video-viewer`. This change updates the media strategy with the new selector.
    
    Ticket: #802
    Ticket URL: https://github.com/beardedspice/beardedspice/issues/802

--- BeardedSpice/MediaStrategies/Udemy.js
+++ BeardedSpice/MediaStrategies/Udemy.js
@@ -4,26 +4,27 @@
 //
 //  Created by Coder-256 on 10/3/15.
 //  Updated by nelsonjchen on 11/24/16.
+//  Updated by rkrv on 07/18/18.
 //  Copyright © 2016 BeardedSpice. All rights reserved.
 //
 BSStrategy = {
-  version:2,
+  version:3,
   displayName:"Udemy",
   accepts: {
     method: "predicateOnTab",
     format:"%K LIKE[c] '*udemy.com*/lecture/*'",
     args: ["URL"]
   },
-  isPlaying: function () {return !(document.querySelector('video-player video').paused);},
+  isPlaying: function () {return !(document.querySelector('video-viewer video').paused);},
   toggle: function () {
-    var theVideo = document.querySelector('video-player video');
+    var theVideo = document.querySelector('video-viewer video');
     if (theVideo.paused) { theVideo.play(); }
     else { theVideo.pause() }
   },
   next: function () {document.querySelector("a.udi.udi-vjs-forward.btn").click()},
   favorite: function () {},
   previous: function () {document.querySelector("a.udi.udi-vjs-rewind.btn").click()},
-  pause: function () {document.querySelector('video-player video').pause();},
+  pause: function () {document.querySelector('video-viewer video').pause();},
   trackInfo: function () {
     return {
       'track': document.querySelector('.course-info__title').textContent,

f12c06717c7af1728d34c4cc2e1aea3870b97965
Igor Deryabin <igor@rodweb.ru>
2018-10-01 17:30:47 +0200

    Update YandexMusic.js

--- BeardedSpice/MediaStrategies/YandexMusic.js
+++ BeardedSpice/MediaStrategies/YandexMusic.js
@@ -16,14 +16,14 @@ BSStrategy = {
   isPlaying: function () {return !!document.querySelector('body.body_show-pause');},
   toggle: function () {document.querySelector('div.b-jambox__play, .player-controls__btn_play').click();},
   next: function () {document.querySelector('div.b-jambox__next, .player-controls__btn_next').click();},
-  favorite: function () { document.querySelector('.player-controls .like.player-controls__btn').click(); },
+  favorite: function () { document.querySelector('.player-controls .d-like.player-controls__btn').click(); },
   previous: function () {document.querySelector('div.b-jambox__prev, .player-controls__btn_prev').click();},
   pause: function () {document.querySelector('.player-controls__btn.player-controls__btn_play').click();},
   trackInfo: function () {
     return {
       track:  document.querySelector('.track.track_type_player .track__title').innerText,
       artist: document.querySelector('.track.track_type_player .track__artists').innerText,
-      favorited: !!document.querySelector('.player-controls__track-controls .like.like_on'),
+      favorited: !!document.querySelector('.player-controls__track-controls .d-like.d-like_on'),
       image: document.querySelector('.album-cover').src.replace('50x50', '600x600')
     };
   }

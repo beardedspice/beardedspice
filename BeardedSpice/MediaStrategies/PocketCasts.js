8e51f0307295a855c3acd814316b0c9573c90f7c
marcozuccaroli <marco.zuccaroli@innoteam.it>
2019-01-16 10:53:27 +0100

    Update PocketCasts Media strategy

--- BeardedSpice/MediaStrategies/PocketCasts.js
+++ BeardedSpice/MediaStrategies/PocketCasts.js
@@ -2,26 +2,25 @@
 //  PocketCasts.plist
 //  BeardedSpice
 //
-//  Created by Dmytro Piliugin on 1/23/15.
-//  Copyright (c) 2015 Tyler Rhodes / Jose Falcon. All rights reserved.
+//  Created by Marco Zuccaroli on 7/17/18.
 //
 BSStrategy = {
-  version:1,
+  version:2,
   displayName:"PocketCasts",
   accepts: {
     method: "predicateOnTab",
     format:"%K LIKE[c] '*play.pocketcasts.com*'",
     args: ["URL"]
   },
-  toggle: function () {document.querySelector('div.play_pause_button').click()},
-  next: function () {document.querySelector('div.skip_forward_button').click()},
-  previous: function () {document.querySelector('div.skip_back_button').click()},
-  pause: function () {document.querySelector('div.pause_button').click()},
+  toggle: function () {document.querySelector('.play_pause_button').click()},
+  next: function () {document.querySelector('.skip_forward_button').click()},
+  previous: function () {document.querySelector('.skip_back_button').click()},
+  pause: function () {document.querySelector('.pause_button').click()},
   trackInfo: function () {
     return {
-      'track': document.querySelector('div.player_top div.player_episode').innerText,
-      'album': document.querySelector('div.player_top div.player_podcast_title').innerText,
-      'image': document.querySelector('div.player_top div.player_artwork img').src,
+      'track': document.querySelector('.controls-center span.player_episode').innerText,
+      'album': document.querySelector('.controls-center span.player_podcast_title').innerText,
+      'image': document.querySelector('.controls-left .podcast-image img').src,
     };
   }
 }

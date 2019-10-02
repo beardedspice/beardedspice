d6984ab635f41262ef8092b8be60eb398726eaac
Roman Sokolov <calm@adguard.com>
2019-10-02 23:14:07 +0300

    added Deezer app support (only playback control)

---
+++ BeardedSpice/Tabs/DeezerTabAdapter.h
@@ -0,0 +1,13 @@
+//
+//  DeezerTabAdapter.h
+//  BeardedSpice
+//
+//  Created by Roman Sokolov on 27.09.19.
+//  Copyright (c) 2019 GPL v3 http://www.gnu.org/licenses/gpl.html
+//
+
+#import "NativeAppTabAdapter.h"
+
+@interface DeezerTabAdapter : NativeAppTabAdapter
+
+@end

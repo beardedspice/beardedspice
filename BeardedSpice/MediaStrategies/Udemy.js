c4bb0285be5fb56ecd91c0ce0ab83fd36d2c028c
shalva-an <s.halvaa26@gmail.com>
2021-01-11 16:42:20 -0500

    Updated Udemy
    Changed: url, video selector, and mapped the correct buttons.

--- BeardedSpice/MediaStrategies/Udemy.js
+++ BeardedSpice/MediaStrategies/Udemy.js
@@ -4,30 +4,45 @@
 //
 //  Created by Coder-256 on 10/3/15.
 //  Updated by nelsonjchen on 11/24/16.
-//  Copyright © 2016 BeardedSpice. All rights reserved.
+//  Updated by shalva-an on 01/11/2021
+//  Copyright (c) 2015-2021 GPL v3 http://www.gnu.org/licenses/gpl.html
+//  Copyright © 2021 BeardedSpice. All rights reserved.
 //
+
 BSStrategy = {
-  version:2,
+  version: 4,
   displayName:"Udemy",
   accepts: {
     method: "predicateOnTab",
-    format:"%K LIKE[c] '*udemy.com*/lecture/*'",
+    format:"%K LIKE[c] '*udemy.com*/course/*'",
     args: ["URL"]
   },
-  isPlaying: function () {return !(document.querySelector('video-player video').paused);},
-  toggle: function () {
-    var theVideo = document.querySelector('video-player video');
-    if (theVideo.paused) { theVideo.play(); }
-    else { theVideo.pause() }
+  isPlaying: function () {
+    return !document.querySelector("video").paused;
   },
-  next: function () {document.querySelector("a.udi.udi-vjs-forward.btn").click()},
-  favorite: function () {},
-  previous: function () {document.querySelector("a.udi.udi-vjs-rewind.btn").click()},
-  pause: function () {document.querySelector('video-player video').pause();},
+  toggle: function () {
+    const theVideo = document.querySelector("video");
+    theVideo.paused ? theVideo.play() : theVideo.pause();
+  },
+  next: function () {
+    document.querySelector('[data-purpose^="forward-skip-button"]').click()
+  },
+  previous: function () {
+    document.querySelector('[data-purpose^="rewind-skip-button"]').click()
+
+  },
+  pause: function () {
+    document.querySelector("video").pause();
+  },
   trackInfo: function () {
     return {
-      'track': document.querySelector('.course-info__title').textContent,
-      'album': 'Udemy'
-    }
-  }
-}
+      track: document.querySelector('[data-purpose^="course-header-title"]').innerText,
+      album: "Udemy",
+    };
+  },
+};
+
+// class names as of 2021 instead of using data-purpose
+// track: document.querySelector('.course-info__title').textContent,
+// document.querySelector(".udi-exp-skip-forward").click();
+// document.querySelector(".udi-exp-skip-back").click();
